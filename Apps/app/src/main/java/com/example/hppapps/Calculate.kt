2a90f07f4177f14bacab59b6aa2fa08f9d6a6caa
raytumb <raytumb.design@gmail.com>
2021-05-21 14:50:28 +0700

    masih ver. java

--- Apps/app/src/main/java/com/example/hppapps/Calculate.kt
+++ Apps/app/src/main/java/com/example/hppapps/Calculate.kt
@@ -1,56 +1,85 @@
 package com.example.hppapps
 
-public class konversiSatuan{
-    //Ambil Jumlah
-    double konversi = 3;
-    System.out.println("Jumlah = " + konversi);
+import java.util.stream.*;
 
-    //Ambil Satuan
-    String satuan = "miligram";
-    System.out.println("Satuan = " + satuan);
+public class HelloWorld{
 
-    if (satuan == "ton"){
-        konversi = konversi * 1000000000; //Ton -> mg
-    } if (satuan == "kg"){
-        konversi = konversi * 1000000; //Kilogram -> mg
-    } if (satuan == "gram"){
-        konversi = konversi * 1000; //Gram -> mg
-    } if (satuan == "lusin"){
-        konversi = konversi * 12; //Lusin -> piece
-    } if (satuan == "rim"){
-        konversi = konversi * 500; //Rim -> piece
-    } if (satuan == "litre"){
-        konversi = konversi * 1000; //Liter -> ml
-    } if (satuan == "spoon"){
-        konversi = konversi * 15; //Sendok makan -> ml
-    } if (satuan == "teaspoon"){
-        konversi = konversi * 45; //Sendok teh -> ml
-    }
-}
+    public static void main(String []args){
+        //Koversi satuan
 
-//RUMUS UTAMA Harga Resep
-//Harga Barang / Jumlah Barang * Jumlah Resep
-class CalculateResep(){
-    double jumlahResep = arrJumlahResep[];
-    double jumlahBarang = arrJumlahBarang[];
-    double hargaBarang = arrHargaBarang[];
+        //----------------
+        //Konversi Resep
+        //----------------
 
-    double hargaResep = hargaBarang / jumlahBarang * jumlahResep;
-    public static int findSumWithoutUsingStream(int[] array) {
-        int sum = 0;
-        for (int value : array) {
-            sum += value;
+        //Input Resep
+        double jumlahResepSatuan = 20;
+
+        //Input Satuan Resep
+        String satuanResep = "mg";
+        System.out.println("Resep: " + jumlahResepSatuan + " " + satuanResep);
+
+        //panggil class konversi
+        //masukkan ke dalam array arrayResepSatuan
+
+
+
+        //----------------
+        //Konversi Barang
+        //----------------
+
+        //Input Harga Barang
+        double hargaBeliSatuan = 5000;
+        System.out.println("Harga Beli Barang: Rp. " + hargaBeliSatuan);
+
+        //Input Jumlah Barang
+        double jumlahBeliSatuan = 350;
+
+        //Input Satuan Barang
+        String satuanBarang = "gram";
+        System.out.println("Jumlah Beli Barang: " + jumlahBeliSatuan + " " + satuanBarang);
+
+        //panggil class konversi
+        //masukkan ke dalam array arrayResepSatuan
+
+
+        //public class konversiSatuan
+        //Ambil Jumlah
+        double konversi = jumlahBeliSatuan;
+
+        //Ambil Satuan
+        String satuan = satuanBarang;
+
+        if (satuan == "ton"){
+            konversi = konversi * 1000000000; //Ton -> mg
+        } if (satuan == "kg"){
+            konversi = konversi * 1000000; //Kilogram -> mg
+        } if (satuan == "gram"){
+            konversi = konversi * 1000; //Gram -> mg
+        } if (satuan == "lusin"){
+            konversi = konversi * 12; //Lusin -> piece
+        } if (satuan == "rim"){
+            konversi = konversi * 500; //Rim -> piece
+        } if (satuan == "litre"){
+            konversi = konversi * 1000; //Liter -> ml
+        } if (satuan == "spoon"){
+            konversi = konversi * 15; //Sendok makan -> ml
+        } if (satuan == "teaspoon"){
+            konversi = konversi * 45; //Sendok teh -> ml
         }
-        return sum;
-    }
-    sigmaHargaResep =
-}
 
-//Sum Harga Produk
-fun hitungHargaProduk(array: IntArray): Int {
-    var hargaProduk = 0
-    for (value in array) {
-        hargaProduk += value
+        System.out.println("\nKonversi = " + konversi + " dari satuan " + satuan);
+
+
+        //public class KalkulasiResep
+        double hargaResepSatuan = hargaBeliSatuan / jumlahBeliSatuan * jumlahResepSatuan;
+
+        System.out.println("\nHarga resep = Rp. " + hargaResepSatuan);
+
+        //Sum all hargaResepSatuan (Dynamic Array)
+        double[] hargaResep = {10, 50, 30, 20};
+
+        double hargaBarang = DoubleStream.of(hargaResep).sum();
+        System.out.println("\nHarga per Barang = Rp. " + hargaBarang);
     }
-    return hargaProduk
+
 }
