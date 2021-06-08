8cb26ff26ae97f93d73bc2e570de6ec6372239ba
raytumb <raytumb.design@gmail.com>
2021-06-08 21:31:36 +0700

    kalkulate
    java = Kalkulate
    kotlin = Calculate

--- Apps/app/src/main/java/com/example/hppapps/Calculate.kt
+++ Apps/app/src/main/java/com/example/hppapps/Calculate.kt
@@ -1,128 +1,138 @@
 package com.example.hppapps
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.util.stream.*;
 import java.util.Arrays;
 
-public class Calculate{
-    public static void main(String []args){
-
-        kalkulasi convert = new kalkulasi();
-        int i;
+object Calculate{
+    @RequiresApi(Build.VERSION_CODES.N)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val convert = Kalkulasi
+        var i: Int
         //Change maximum array size:
-        int arraySize = 15;
-        double x;
+        val arraySize = 15
+        var x: Double
 
         //Koversi satuan------------------------------
 
         //----------------
         //Konversi Resep
-        System.out.println("\n//Resep//");
+        println("\n//Resep//")
         //----------------
 
         //--Input Resep
-        double[] arrayJumlahResepSatuan = new double[arraySize];
-        arrayJumlahResepSatuan [0] = 20;
-        arrayJumlahResepSatuan [1] = 20;
-        arrayJumlahResepSatuan [2] = 20;
+        val arrayJumlahResepSatuan = DoubleArray(arraySize)
+        //arrayJumlahResepSatuan[0] = 20.0
+        //arrayJumlahResepSatuan[1] = 20.0
+        //arrayJumlahResepSatuan[2] = 20.0
 
         //--Input Satuan Resep
-        String[] arraySatuanResep = new String[arraySize];
-        arraySatuanResep [0] = "gram";
-        arraySatuanResep [1] = "gram";
-        arraySatuanResep [2] = "gram";
+        val arraySatuanResep = arrayOfNulls<String>(arraySize)
+        arraySatuanResep[0] = "gram"
+        arraySatuanResep[1] = "gram"
+        arraySatuanResep[2] = "gram"
 
         //Loop Array Class Konversi
-        double[] arrayKonversiResep = new double[arraySize];
-        for (i = 0; i < arrayJumlahResepSatuan.length; i++) {
-            //Akses setiap anggota array
-            arrayKonversiResep[i] = convert.konversiSatuan(arrayJumlahResepSatuan[i], arraySatuanResep[i]);
+        val arrayKonversiResep = DoubleArray(arraySize)
+        i = 0
+        while (i < arrayJumlahResepSatuan.size) {
 
-            x = arrayKonversiResep[i];
-            System.out.print(x + " ");
+            //Akses setiap anggota array
+            arrayKonversiResep[i] = convert.KonversiSatuan(arrayJumlahResepSatuan[i], arraySatuanResep[i])
+            x = arrayKonversiResep[i]
+            print("$x ")
+            i++
         }
 
         //----------------
         //Konversi Barang
-        System.out.println("\n\n/Barang//");
+        println("\n\n/Barang//")
         //----------------
 
         //--Input Harga Barang
-        double[] arrayHargaBeliSatuan = new double[arraySize];
-        arrayHargaBeliSatuan [0] = 5000;
-        arrayHargaBeliSatuan [1] = 5000;
-        arrayHargaBeliSatuan [2] = 5000;
+        val arrayHargaBeliSatuan = DoubleArray(arraySize)
+        //arrayHargaBeliSatuan[0] = 5000
+        //arrayHargaBeliSatuan[1] = 5000
+        //arrayHargaBeliSatuan[2] = 5000
 
         //--Input Jumlah Barang
-        double[] arrayJumlahBeliSatuan = new double[arraySize];
-        arrayJumlahBeliSatuan [0] = 350;
-        arrayJumlahBeliSatuan [1] = 350;
-        arrayJumlahBeliSatuan [2] = 350;
+        val arrayJumlahBeliSatuan = DoubleArray(arraySize)
+        //arrayJumlahBeliSatuan[0] = 350
+        //arrayJumlahBeliSatuan[1] = 350
+        //arrayJumlahBeliSatuan[2] = 350
 
         //--Input Satuan Barang
-        String[] arraySatuanBarang = new String[arraySize];
-        arraySatuanBarang[0] = "gram";
-        arraySatuanBarang[1] = "gram";
-        arraySatuanBarang[2] = "gram";
+        val arraySatuanBarang = arrayOfNulls<String>(arraySize)
+        //arraySatuanBarang[0] = "gram"
+        //arraySatuanBarang[1] = "gram"
+        //arraySatuanBarang[2] = "gram"
 
         //-----Memanggil Class Konversi
+        val arrayKonversiSatuanBarang = DoubleArray(arraySize)
+        i = 0
+        while (i < arrayJumlahResepSatuan.size) {
 
-        double[] arrayKonversiSatuanBarang = new double[arraySize];
-        for (i = 0; i < arrayJumlahResepSatuan.length; i++) {
             //Akses setiap anggota array
-            arrayKonversiSatuanBarang[i] = convert.konversiSatuan(arrayJumlahBeliSatuan[i], arraySatuanBarang[i]);
-
-            x = arrayKonversiSatuanBarang[i];
-            System.out.print(x + " ");
+            arrayKonversiSatuanBarang[i] = arraySatuanBarang[i]?.let { convert.KonversiSatuan(arrayJumlahBeliSatuan[i], it) }!!
+            x = arrayKonversiSatuanBarang[i]
+            print("$x ")
+            i++
         }
+        println("\n\n//Harga Resep Satuan//")
+        val arrayResepSatuan = DoubleArray(arraySize)
+        i = 0
+        while (i < arrayResepSatuan.size) {
 
-        System.out.println("\n\n//Harga Resep Satuan//");
-        double[] arrayResepSatuan = new double[arraySize];
-        for (i = 0; i < arrayResepSatuan.length; i++) {
             //Akses setiap anggota array
-            arrayResepSatuan[i] = convert.kalkulasiResep(arrayHargaBeliSatuan[i], arrayKonversiSatuanBarang[i], arrayKonversiResep[i]);
-
-            x = arrayResepSatuan[i];
-            System.out.print(x + " ");
+            arrayResepSatuan[i] = convert.KalkulasiResep(arrayHargaBeliSatuan[i], arrayKonversiSatuanBarang[i], arrayKonversiResep[i])
+            x = arrayResepSatuan[i]
+            print("$x ")
+            i++
         }
 
         //Masukkan hasil kalkulasi resep
-        System.out.println("\n\n//Final (Harga Produk)//");
-        double hargaProduk = DoubleStream.of(arrayResepSatuan).sum();
-        System.out.println("Harga akhir: Rp. " + hargaProduk);
-
+        println("\n\n//Final (Harga Produk)//")
+        val hargaProduk: Double = DoubleStream.of(arrayResepSatuan).sum()
+        println("Harga akhir: Rp. $hargaProduk")
     }
 }
 
-class kalkulasi{
-    static double konversiSatuan(double konversi, String satuan){
-
-        if (satuan == "ton"){
-            konversi = konversi * 1000000000; //Ton -> mg
-        } if (satuan == "kg"){
-            konversi = konversi * 1000000; //Kilogram -> mg
-        } if (satuan == "gram"){
-            konversi = konversi * 1000; //Gram -> mg
-        } if (satuan == "lusin"){
-            konversi = konversi * 12; //Lusin -> piece
-        } if (satuan == "rim"){
-            konversi = konversi * 500; //Rim -> piece
-        } if (satuan == "litre"){
-            konversi = konversi * 1000; //Liter -> ml
-        } if (satuan == "spoon"){
-            konversi = konversi * 15; //Sendok makan -> ml
-        } if (satuan == "teaspoon"){
-            konversi = konversi * 45; //Sendok teh -> ml
+internal object Kalkulasi {
+    fun KonversiSatuan(konversi: Double, satuan: String): Double {
+        if (satuan === "ton") {
+            konversi *= 1000000000 //Ton -> mg
         }
-
-        return konversi;
+        if (satuan === "kg") {
+            konversi *= 1000000 //Kilogram -> mg
+        }
+        if (satuan === "gram") {
+            konversi *= 1000 //Gram -> mg
+        }
+        if (satuan === "lusin") {
+            konversi *= 12 //Lusin -> piece
+        }
+        if (satuan === "rim") {
+            konversi *= 500 //Rim -> piece
+        }
+        if (satuan === "litre") {
+            konversi *= 1000 //Liter -> ml
+        }
+        if (satuan === "spoon") {
+            konversi *= 15 //Sendok makan -> ml
+        }
+        if (satuan === "teaspoon") {
+            konversi *= 45 //Sendok teh -> ml
+        }
+        return konversi
     }
 
-    static double kalkulasiResep(double hargaBeliSatuan, double jumlahBeliSatuan, double jumlahResepSatuan){
-        if (jumlahBeliSatuan == 0){
-            jumlahBeliSatuan = 1;
+    fun KalkulasiResep(hargaBeliSatuan: Double, jumlahBeliSatuan: Double, jumlahResepSatuan: Double): Double {
+        var jumlahBeliSatuan = jumlahBeliSatuan
+        if (jumlahBeliSatuan == 0.0) {
+            jumlahBeliSatuan = 1.0
         }
-        double hargaResepSatuan = hargaBeliSatuan / jumlahBeliSatuan * jumlahResepSatuan;
-
-        return hargaResepSatuan;
+        return hargaBeliSatuan / jumlahBeliSatuan * jumlahResepSatuan
     }
 }
---
+++ Apps/app/src/main/java/com/example/hppapps/Kalkulate.java
@@ -0,0 +1,127 @@
+package com.example.hppapps;
+
+import java.util.stream.*;
+import java.util.Arrays;
+
+public class Kalkulate{
+    public static void main(String []args){
+
+        Kalkulasi convert = new Kalkulasi();
+        FromInput fromInput = new FromInput();
+        private int i;
+        //Change maximum array size:
+        int arraySize = 15;
+        double x;
+
+        //Koversi satuan------------------------------
+
+        //----------------
+        //Konversi Resep
+        System.out.println("\n//Resep//");
+        //----------------
+
+        //--Input Resep
+        public double[] arrayJumlahResepSatuan = new double[arraySize];
+        arrayJumlahResepSatuan [0] = 50; //Cabe keriting
+        arrayJumlahResepSatuan [1] = 25; //Cabe rawit
+        arrayJumlahResepSatuan [2] = 14; //Bawang putih
+
+        //--Input Satuan Resep
+        public String[] arraySatuanResep = new String[arraySize];
+        arraySatuanResep [0] = "gram";
+        arraySatuanResep [1] = "gram";
+        arraySatuanResep [2] = "gram";
+
+        //Loop Array Class Konversi
+        public double[] arrayKonversiResep = new double[arraySize];
+        for (i = 0; i < arrayJumlahResepSatuan.length; i++) {
+            //Akses setiap anggota array
+            arrayKonversiResep[i] = convert.konversiSatuan(arrayJumlahResepSatuan[i], arraySatuanResep[i]);
+
+            x = arrayKonversiResep[i];
+            System.out.print(x + " ");
+        }
+        //----------------
+        //Konversi Barang
+        System.out.println("\n\n//Barang//");
+        //----------------
+
+        //--Input Harga Barang
+        public double[] arrayHargaBeliSatuan = new double[arraySize];
+        arrayHargaBeliSatuan [0] = 35000;
+        arrayHargaBeliSatuan [1] = 30000;
+        arrayHargaBeliSatuan [2] = 30000;
+
+        //--Input Jumlah Barang
+        public double[] arrayJumlahBeliSatuan = new double[arraySize];
+        arrayJumlahBeliSatuan [0] = 1;
+        arrayJumlahBeliSatuan [1] = 1;
+        arrayJumlahBeliSatuan [2] = 1;
+
+        //--Input Satuan Barang
+        public String[] arraySatuanBarang = new String[arraySize];
+        arraySatuanBarang[0] = "kg";
+        arraySatuanBarang[1] = "kg";
+        arraySatuanBarang[2] = "kg";
+
+        //-----Memanggil Class Konversi
+
+        public double[] arrayKonversiSatuanBarang = new double[arraySize];
+        for (i = 0; i < arrayJumlahResepSatuan.length; i++) {
+            //Akses setiap anggota array
+            arrayKonversiSatuanBarang[i] = convert.konversiSatuan(arrayJumlahBeliSatuan[i], arraySatuanBarang[i]);
+
+            x = arrayKonversiSatuanBarang[i];
+            System.out.print(x + " ");
+        }
+
+        System.out.println("\n\n//Harga Resep Satuan//");
+        double[] arrayResepSatuan = new double[arraySize];
+        for (i = 0; i < arrayResepSatuan.length; i++) {
+            //Akses setiap anggota array
+            arrayResepSatuan[i] = convert.kalkulasiResep(arrayHargaBeliSatuan[i], arrayKonversiSatuanBarang[i], arrayKonversiResep[i]);
+
+            x = arrayResepSatuan[i];
+            System.out.print(x + " ");
+        }
+
+        //Masukkan hasil kalkulasi resep
+        System.out.println("\n\n//Final (Harga Produk)//");
+        double hargaProduk = DoubleStream.of(arrayResepSatuan).sum();
+        System.out.println("Harga akhir: Rp. " + hargaProduk);
+
+    }
+}
+
+class Kalkulasi{
+    static double konversiSatuan(double konversi, String satuan){
+
+        if (satuan == "ton"){
+            konversi = konversi * 1000000000; //Ton -> mg
+        } if (satuan == "kg"){
+            konversi = konversi * 1000000; //Kilogram -> mg
+        } if (satuan == "gram"){
+            konversi = konversi * 1000; //Gram -> mg
+        } if (satuan == "lusin"){
+            konversi = konversi * 12; //Lusin -> piece
+        } if (satuan == "rim"){
+            konversi = konversi * 500; //Rim -> piece
+        } if (satuan == "litre"){
+            konversi = konversi * 1000; //Liter -> ml
+        } if (satuan == "sdm"){
+            konversi = konversi * 15; //Sendok makan -> ml
+        } if (satuan == "sdt"){
+            konversi = konversi * 45; //Sendok teh -> ml
+        }
+        return konversi;
+    }
+
+    static double kalkulasiResep(double hargaBeliSatuan, double jumlahBeliSatuan, double jumlahResepSatuan){
+        if (jumlahBeliSatuan == 0){
+            jumlahBeliSatuan = 1;
+        }
+        double hargaResepSatuan = hargaBeliSatuan / jumlahBeliSatuan * jumlahResepSatuan;
+
+        return hargaResepSatuan;
+    }
+}
